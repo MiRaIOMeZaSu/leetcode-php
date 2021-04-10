a22472806e408ce6131cb5430c7f6a3f9596cf02
miraiomezasu <3792927266@qq.com>
2021-04-10 20:59:19 +0800

    1658: done

--- 1658_minOperations.php
+++ 1658_minOperations.php
@@ -14,32 +14,43 @@ class Solution
 
     function minOperations($nums, $x)
     {
-        // 使用回溯法
-        // 在操作数大于已有最小值时及时剪枝
-        // 小于零时剪枝
-        $this->size = count($nums);
-        $this->nums = &$nums;
+        // 进和退
         $this->ret = PHP_INT_MAX;
-        $this->solve(0, $this->size - 1, $x);
-        return $this->ret != PHP_INT_MAX ? $this->ret : -1;
-    }
-
-    function solve($left, $right, $rest)
-    {
-        if ($rest < 0) {
-            return;
-        } else if ($rest > $this->ret) {
-            return;
-        } else if ($rest == 0) {
+        $this->size = count($nums);
+        $left = 0;
+        $right = $this->size - 1;
+        $num = 0;
+        while ($left <= $right && $num < $x) {
+            $num += $nums[$left];
+            $left++;
+        }
+        if ($num == $x) {
             $this->ret = min($left + ($this->size - 1 - $right), $this->ret);
         }
+        $num -= $nums[$left - 1];
+        $left--;
         if ($left <= $right) {
-            $this->solve($left, $right - 1, $rest - $this->nums[$right]);
-            $this->solve($left + 1, $right, $rest - $this->nums[$left]);
+            while ($left <= $right && $num < $x) {
+                $num += $nums[$right];
+                $right--;
+                while ($num > $x && $left >= 0) {
+                    $num -= $nums[$left - 1];
+                    $left--;
+                }
+                if ($num == $x) {
+                    $this->ret = min($left + ($this->size - 1 - $right), $this->ret);
+                    $num -= $nums[$left - 1];
+                    $left--;
+                }
+            }
+        } else {
+            return -1;
         }
+        return $this->ret != PHP_INT_MAX ? $this->ret : -1;
     }
 }
 
 $solution = new Solution();
-$arr = [3, 2, 20, 1, 1, 3];
-$solution->minOperations($arr, 10);
+$arr = [1, 1, 4, 2, 3];
+$ret = $solution->minOperations($arr, 5);
+echo $ret;
